680251920626020f85fdb1e9854fb2b97117853d
mus0u <musou@musou.co.uk>
2016-05-19 19:54:09 -0400

    lol this sht does not work

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,110 @@
 extern crate rand;
-
-#[derive(Copy, Clone)]
-pub enum Tile {
-  Empty,
-  Tree,
-  Burning,
-}
-
-const GROW_PROB: u32 = 10;
-const INITIAL_TREE_PROB: u32 = 50;
-const FIRE_PROB: u32 = 5;
-const MAX_GENERATIONS: u32 = 30;
-
+extern crate ansi_term;
 use Tile::{Empty, Tree, Burning};
 use rand::Rng;
 use std::cmp::Ordering;
+use std::time::Duration;
+use std::fmt;
+use ansi_term::Colour::*;
 
-fn main() {
+const GROW_PROB: u32 = 2;
+const INITIAL_TREE_PROB: u32 = 20;
+const FIRE_PROB: u32 = 5;
+const MAX_GENERATIONS: u32 = 31;
 
-  let mut tile = if prob_check(INITIAL_TREE_PROB) { Tree } else { Empty } ;
-
-  report(tile);
-
-  for _ in 1..MAX_GENERATIONS {
-    tile = match tile {
-      Empty => {
-        if prob_check(GROW_PROB) == true {
-          Tree
-        } else {
-          tile
+#[derive(Copy, Clone, Eq, PartialEq)]
+pub enum Tile {
+    Empty,
+    Tree,
+    Burning,
+}
+impl fmt::Display for Tile {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Empty => write!(f, " "),
+            Tree => write!(f, "{}", Green.paint("T")),
+            Burning => write!(f, "{}", Red.paint("#"))
         }
-      },
-      Tree => {
-        if prob_check(FIRE_PROB) == true {
-          Burning
-        } else {
-          tile
-        }
-      }
-      _ => tile,
-    };
-
-    report(tile);
-  }
-
+    }
 }
 
-fn report(tile: Tile) {
-  match tile {
-    Empty => println!("no tree"),
-    Tree => println!("tree"),
-    Burning => println!("fire!"),
-  }
+fn main() {
+    let mut tiles = [[Empty; 30]; 30];
+    for row in tiles.iter_mut() {
+        for tile in row.iter_mut() {
+            *tile = if prob_check(INITIAL_TREE_PROB) { Tree } else { Empty } ;
+        }
+    }
+    let sleep_time = Duration::new(0,500000000); // 1/2 seconds
+    let mut y = 0;
+    for gen in 1..MAX_GENERATIONS {
+        println!("{}", gen);
+        let mut x = 0;
+        for row in tiles.iter() {
+            for tile in row.iter() {
+                let mut neighbor_is_burning = false;
+                if ((x - 1) >= 0) && ((y - 1) >= 0) {
+                    // SAAAAD FACE
+                    if tiles[y - 1][x - 1] == Burning { neighbor_is_burning = true }
+                }
+                // if (y - 1) >= 0 {
+                //     if tiles[y - 1][x] == Burning { neighbor_is_burning = true }
+                // }
+                // if (x + 1) < 30 {
+                //     if tiles[y - 1][x + 1] == Burning { neighbor_is_burning = true }
+                // }
+                // if (x - 1) >= 0 {
+                //     if tiles[y][x - 1] == Burning { neighbor_is_burning = true }
+                // }
+                // if (x + 1) < 30 {
+                //     if tiles[y][x + 1] == Burning { neighbor_is_burning = true }
+                // }
+                // if (x - 1) >= 0 && (y + 1) < 30 {
+                //     if tiles[y + 1][x - 1] == Burning { neighbor_is_burning = true }
+                // }
+                // if (y + 1) < 30 {
+                //     if tiles[y + 1][x] == Burning { neighbor_is_burning = true }
+                // }
+                // if (x + 1) < 30 && (y + 1) < 30 {
+                //     if tiles[y + 1][x + 1] == Burning { neighbor_is_burning = true }
+                // }
+                *tile = modify_tile(*tile, neighbor_is_burning);
+                print!("{}", tile);
+                x += 1;
+            }
+            println!("");
+            y += 1;
+        }
+        std::thread::sleep(sleep_time);
+        clear_screen();
+    }
+}
+
+fn modify_tile(tile: Tile, neighbor_is_burning: bool) -> Tile {
+    match tile {
+        Empty => {
+            if prob_check(GROW_PROB) == true {
+                Tree
+            } else {
+                tile
+            }
+        },
+        Tree => {
+            if prob_check(FIRE_PROB) == true || neighbor_is_burning {
+                Burning
+            } else {
+                tile
+            }
+        }
+        Burning => Empty
+    }
+}
+
+fn clear_screen() {
+    print!("{}[2J", 27 as char);
 }
 
 fn prob_check(chance: u32) -> bool {
-  let roll: u32 = rand::thread_rng().gen_range(1, 101);
-  if roll.cmp(&chance) == Ordering::Less { true } else { false }
+    let roll: u32 = rand::thread_rng().gen_range(1, 101);
+    if roll.cmp(&chance) == Ordering::Less { true } else { false }
 }
-
-
